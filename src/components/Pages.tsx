447d3d41f083fc8c96ce33dda2e1b5cd69150681
Brendin Wright <brendin100@gmail.com>
2024-11-20 20:49:31 -0600

    Uninstall bootstrap lol I'm not usin dat

---
+++ src/components/Pages.tsx
@@ -0,0 +1,25 @@
+import './list.css'
+
+const pages = [
+	{ title: 'About Me', href: 'temp', id: 1 },
+	{ title: 'Portfolio', href: 'temp', id: 2 },
+	{ title: 'Hobby Stuff', href: 'temp', id: 3 },
+];
+
+export default function Pages() {
+	const listPages = pages.map(page =>
+		<li className = 'menu-option' key={page.id}>
+			<a href={page.href}>
+				{page.title}
+			</a>
+		</li>
+	)
+
+	return (
+		<div>
+			<ul>
+				{listPages}
+			</ul>
+		</div>
+	);
+}
